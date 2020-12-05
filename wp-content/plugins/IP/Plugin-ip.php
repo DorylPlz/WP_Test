b02f2fff4edf5abf61ce9d0ab1737510a83c4967
Daryl Olivares <darylolivares@gmail.com>
2020-12-05 14:56:01 -0300

    Creación de archivo para el plugin de IP

---
+++ wp-content/plugins/IP/Plugin-ip.php
@@ -0,0 +1,5 @@
+//Extra: Durante la caída del servidor se estaba desarrollando un plugin que permitía mostrar en la página, la IP desde donde se conecta el usuario. 
+//Este plugin no se pudo implementar debido a la caída del servidor. 
+//Si es que cuentas con tiempo suficiente para desarrollar esta funcionalidad, sumarías puntos extras con el desafío. 
+//Pero no es un requerimiento para completar el desafio
+
